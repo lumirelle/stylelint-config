f28549b57677ad69f26715e63674969937eb2d9b
Lumirelle <shabbyacc@outlook.com>
2025-09-18 17:07:50 +0800

    chore(tools): update cz config

--- cz.config.js
+++ cz.config.js
@@ -1,17 +1,60 @@
 // @ts-check
+import fs from 'node:fs'
+import path from 'node:path'
 import { definePrompt } from 'czg'
 
+/* -------------------------- Config for generation ------------------------- */
+const modulesDir = 'src'
+
+/* ---------------------------- Scopes generation --------------------------- */
+/**
+ * @param {string} dir
+ * @return {string[]} names of directories under `dir`
+ */
+function getModuleNames(dir) {
+  return fs.readdirSync(path.resolve(import.meta.dirname, dir)).filter((file) => {
+    return fs.statSync(path.resolve(import.meta.dirname, dir, file)).isDirectory()
+  })
+}
+
+/**
+ * @param {string} str
+ * @param {number} length
+ * @return {string} `str` if `str.length >= length`, else `str` padded with spaces to `length`
+ */
+function formatName(str, length) {
+  if (str.length >= length)
+    return str
+  return str + ' '.repeat(length - str.length)
+}
+
+const modules = getModuleNames(modulesDir)
+const maxLenModuleName = Math.max(...modules.map(mod => mod.length)) + 1 // +1 for `:`
+const moduleScopes = modules.map(mod => ({
+  value: mod,
+  name: `${formatName(`${mod}:`, maxLenModuleName)} ${modulesDir}/${mod}`,
+}))
+
+/* --------------------------------- Config --------------------------------- */
+
 export default definePrompt({
-  markBreakingChangeMode: true,
-  allowBreakingChanges: ['feat', 'fix', 'chore'],
   alias: {
     typo: 'docs: fix typos',
     readme: 'docs: update README.md',
+    deps: 'chore: update dependencies',
   },
+
+  scopes: moduleScopes,
   scopeOverrides: {
     chore: [
-      'deps',
-      'tools',
+      ...moduleScopes,
+      { value: 'deps', name: `${formatName('deps: ', maxLenModuleName)} A dependencies change` },
+      { value: 'tools', name: `${formatName('tools: ', maxLenModuleName)} A tools and utilities change` },
     ],
   },
+
+  allowBreakingChanges: ['feat', 'fix', 'chore'],
+  markBreakingChangeMode: true,
+
+  skipQuestions: ['footerPrefix', 'footer', 'confirmCommit'],
 })
