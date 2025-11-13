5a636216839fa9a5d06aeb3f92cad839d5bfdca1
Lumirelle <shabbyacc@outlook.com>
2025-11-13 22:17:25 +0800

    chore: simplify cz config

--- cz.config.js
+++ cz.config.js
@@ -1,44 +1,5 @@
 // @ts-check
-import fs from 'node:fs'
-import path from 'node:path'
 import { definePrompt } from 'czg'
-
-/* -------------------------- Config for generation ------------------------- */
-const modulesDir = 'src'
-
-/* ---------------------------- Scopes generation --------------------------- */
-/**
- * @param {string} dir
- * @return {string[]} names of directories under `dir`
- */
-function getModuleNames(dir) {
-  if (!dir || dir === '') {
-    return []
-  }
-  return fs.readdirSync(path.resolve(import.meta.dirname, dir)).filter((file) => {
-    return fs.statSync(path.resolve(import.meta.dirname, dir, file)).isDirectory() && !file.match(/^\.|node_modules/)
-  })
-}
-
-/**
- * @param {string} str
- * @param {number} length
- * @return {string} `str` if `str.length >= length`, else `str` padded with spaces to `length`
- */
-function formatName(str, length) {
-  if (str.length >= length)
-    return str
-  return str + ' '.repeat(length - str.length)
-}
-
-const modules = getModuleNames(modulesDir)
-const maxLenModuleName = Math.max(...modules.map(mod => mod.length)) + 1 // +1 for `:`
-const moduleScopes = modules.map(mod => ({
-  value: mod,
-  name: `${formatName(`${mod}:`, maxLenModuleName)} ${modulesDir}/${mod}`,
-}))
-
-/* --------------------------------- Config --------------------------------- */
 
 export default definePrompt({
   alias: {
@@ -46,8 +7,6 @@ export default definePrompt({
     readme: 'docs: update README.md',
     deps: 'chore: update dependencies',
   },
-
-  scopes: moduleScopes,
 
   allowBreakingChanges: ['feat', 'fix', 'chore'],
   markBreakingChangeMode: true,
