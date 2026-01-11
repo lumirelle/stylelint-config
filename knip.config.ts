e473442dccd4c6d68ea19261bd335940205a4a3f
Lumirelle <shabbyacc@outlook.com>
2026-01-11 23:17:31 +0800

    chore: remove unexpected deps

--- knip.config.ts
+++ knip.config.ts
@@ -1,13 +1,17 @@
 import type { KnipConfig } from 'knip'
 
+// @keep-sorted
 export default {
   // @keep-sorted
   ignoreDependencies: [
-    '@antfu/utils',
     '@arethetypeswrong/core',
     'bumpp',
     'lint-staged',
     'publint',
     'tsdown',
+  ],
+  // @keep-sorted
+  ignoreFiles: [
+    'fixtures/**',
   ],
 } satisfies KnipConfig
