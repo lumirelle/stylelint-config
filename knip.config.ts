f95b91f4a2e95abff2a9be2c281b570938daa298
Lumirelle <shabbyacc@outlook.com>
2026-01-12 11:43:33 +0800

    chore: update knip config

--- knip.config.ts
+++ knip.config.ts
@@ -5,9 +5,16 @@ export default {
   // @keep-sorted
   ignoreDependencies: [
     '@arethetypeswrong/core',
+    '@dreamsicle.io/stylelint-config-tailwindcss',
+    '@stylistic/stylelint-config',
     'bumpp',
     'lint-staged',
+    'postcss-less',
     'publint',
+    'stylelint-config-html',
+    'stylelint-config-recess-order',
+    'stylelint-less',
+    'stylelint-scss',
     'tsdown',
   ],
   // @keep-sorted
