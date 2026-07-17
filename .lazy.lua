aebf20cbcce3c0e77afbffa0a29ae13f0cf10d50
Lumirelle <shabbyacc@outlook.com>
2026-07-17 15:11:05 +0800

    chore: update eslint setup for lazyvim

--- .lazy.lua
+++ .lazy.lua
@@ -1,22 +1,21 @@
 -- ESLint as formatter
 local eslint_support_filetypes = {
-  -- JS & TS
+  -- Code
   "javascript",
   "javascriptreact",
   "typescript",
   "typescriptreact",
-  -- Markdown
-  "markdown",
-  -- JSON
+  "vue",
+  -- Configurations
   "json",
   "jsonc",
-  -- YAML
   "yaml",
-  -- TOML
   "toml",
+  -- Documentations
+  "markdown",
 }
 local eslint_support_file_patterns = {
-  -- JS & TS
+  -- Code
   "*.js",
   "*.mjs",
   "*.cjs",
@@ -25,18 +24,17 @@ local eslint_support_file_patterns = {
   "*.mts",
   "*.cts",
   "*.tsx",
-  -- Markdown
+  "*.vue",
+  -- Configurations
+  "*.json",
+  "*.jsonc",
+  "*.yaml",
+  "*.yml",
+  "*.toml",
+  -- Documentations
   "*.md",
   "*.mdc",
   "*.mdx",
-  -- JSON
-  "*.json",
-  "*.jsonc",
-  -- YAML
-  "*.yaml",
-  "*.yml",
-  -- TOML
-  "*.toml",
 }
 -- Disable other formatter conflict with ESLint
 require("snacks").util.lsp.on(function(_, client)
